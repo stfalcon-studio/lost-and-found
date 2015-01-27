9b7dfe9d89e2cf48bf153074e638aa650994f5c1
LogansUA <logansoleg@gmail.com>
2015-01-27 14:36:01 +0200

    Add symfony skeleton

---
+++ app/cache/dev/twig/7b/3a/bf08852e83f9cdd42cceb531fc6c2e519c17746f16e98bb140ec8157b977.php
@@ -0,0 +1,188 @@
+<?php
+
+/* AcmeDemoBundle:Welcome:index.html.twig */
+class __TwigTemplate_7b3abf08852e83f9cdd42cceb531fc6c2e519c17746f16e98bb140ec8157b977 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        try {
+            $this->parent = $this->env->loadTemplate("AcmeDemoBundle::layout.html.twig");
+        } catch (Twig_Error_Loader $e) {
+            $e->setTemplateFile($this->getTemplateName());
+            $e->setTemplateLine(1);
+
+            throw $e;
+        }
+
+        $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'content_header' => array($this, 'block_content_header'),
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "AcmeDemoBundle::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = array())
+    {
+        echo "Symfony - Welcome";
+    }
+
+    // line 5
+    public function block_content_header($context, array $blocks = array())
+    {
+        echo "";
+    }
+
+    // line 7
+    public function block_content($context, array $blocks = array())
+    {
+        // line 8
+        echo "    ";
+        $context["version"] = ((twig_constant("Symfony\\Component\\HttpKernel\\Kernel::MAJOR_VERSION") . ".") . twig_constant("Symfony\\Component\\HttpKernel\\Kernel::MINOR_VERSION"));
+        // line 9
+        echo "
+    <h1 class=\"title\">Welcome!</h1>
+
+    <p>Congratulations! You have successfully installed a new Symfony application.</p>
+
+    <div class=\"symfony-blocks-welcome\">
+        <div class=\"block-quick-tour\">
+            <div class=\"illustration\">
+                <img src=\"";
+        // line 17
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/acmedemo/images/welcome-quick-tour.gif"), "html", null, true);
+        echo "\" alt=\"Quick tour\" />
+            </div>
+            <a href=\"http://symfony.com/doc/";
+        // line 19
+        echo twig_escape_filter($this->env, (isset($context["version"]) ? $context["version"] : $this->getContext($context, "version")), "html", null, true);
+        echo "/quick_tour/index.html\" class=\"sf-button sf-button-selected\">
+                <span class=\"border-l\">
+                    <span class=\"border-r\">
+                        <span class=\"btn-bg\">Read the Quick Tour</span>
+                    </span>
+                </span>
+            </a>
+        </div>
+        ";
+        // line 27
+        if (($this->getAttribute((isset($context["app"]) ? $context["app"] : $this->getContext($context, "app")), "environment", array()) == "dev")) {
+            // line 28
+            echo "            <div class=\"block-configure\">
+                <div class=\"illustration\">
+                    <img src=\"";
+            // line 30
+            echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/acmedemo/images/welcome-configure.gif"), "html", null, true);
+            echo "\" alt=\"Configure your application\" />
+                </div>
+                <a href=\"";
+            // line 32
+            echo $this->env->getExtension('routing')->getPath("_configurator_home");
+            echo "\" class=\"sf-button sf-button-selected\">
+                    <span class=\"border-l\">
+                        <span class=\"border-r\">
+                            <span class=\"btn-bg\">Configure</span>
+                        </span>
+                    </span>
+                </a>
+            </div>
+        ";
+        }
+        // line 41
+        echo "        <div class=\"block-demo\">
+            <div class=\"illustration\">
+                <img src=\"";
+        // line 43
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/acmedemo/images/welcome-demo.gif"), "html", null, true);
+        echo "\" alt=\"Demo\" />
+            </div>
+            <a href=\"";
+        // line 45
+        echo $this->env->getExtension('routing')->getPath("_demo");
+        echo "\" class=\"sf-button sf-button-selected\">
+                <span class=\"border-l\">
+                    <span class=\"border-r\">
+                        <span class=\"btn-bg\">Run The Demo</span>
+                    </span>
+                </span>
+            </a>
+        </div>
+    </div>
+
+    <div class=\"symfony-blocks-help\">
+        <div class=\"block-documentation\">
+            <ul>
+                <li><strong>Documentation</strong></li>
+                <li><a href=\"http://symfony.com/doc/";
+        // line 59
+        echo twig_escape_filter($this->env, (isset($context["version"]) ? $context["version"] : $this->getContext($context, "version")), "html", null, true);
+        echo "/book/index.html\">The Book</a></li>
+                <li><a href=\"http://symfony.com/doc/";
+        // line 60
+        echo twig_escape_filter($this->env, (isset($context["version"]) ? $context["version"] : $this->getContext($context, "version")), "html", null, true);
+        echo "/cookbook/index.html\">The Cookbook</a></li>
+                <li><a href=\"http://symfony.com/doc/";
+        // line 61
+        echo twig_escape_filter($this->env, (isset($context["version"]) ? $context["version"] : $this->getContext($context, "version")), "html", null, true);
+        echo "/components/index.html\">The Components</a></li>
+                <li><a href=\"http://symfony.com/doc/";
+        // line 62
+        echo twig_escape_filter($this->env, (isset($context["version"]) ? $context["version"] : $this->getContext($context, "version")), "html", null, true);
+        echo "/reference/index.html\">Reference</a></li>
+                <li><a href=\"http://symfony.com/doc/";
+        // line 63
+        echo twig_escape_filter($this->env, (isset($context["version"]) ? $context["version"] : $this->getContext($context, "version")), "html", null, true);
+        echo "/glossary.html\">Glossary</a></li>
+            </ul>
+        </div>
+        <div class=\"block-documentation-more\">
+            <ul>
+                <li><strong>Sensio</strong></li>
+                <li><a href=\"http://trainings.sensiolabs.com\">Trainings</a></li>
+                <li><a href=\"http://books.sensiolabs.com\">Books</a></li>
+            </ul>
+        </div>
+        <div class=\"block-community\">
+            <ul>
+                <li><strong>Community</strong></li>
+                <li><a href=\"http://symfony.com/irc\">IRC channel</a></li>
+                <li><a href=\"http://symfony.com/mailing-lists\">Mailing lists</a></li>
+                <li><a href=\"http://forum.symfony-project.org\">Forum</a></li>
+                <li><a href=\"http://symfony.com/doc/";
+        // line 79
+        echo twig_escape_filter($this->env, (isset($context["version"]) ? $context["version"] : $this->getContext($context, "version")), "html", null, true);
+        echo "/contributing/index.html\">Contributing</a></li>
+            </ul>
+        </div>
+    </div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "AcmeDemoBundle:Welcome:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  166 => 79,  147 => 63,  143 => 62,  139 => 61,  135 => 60,  131 => 59,  114 => 45,  109 => 43,  105 => 41,  93 => 32,  88 => 30,  84 => 28,  82 => 27,  71 => 19,  66 => 17,  56 => 9,  53 => 8,  50 => 7,  44 => 5,  38 => 3,  11 => 1,);
+    }
+}
---
+++ app/cache/dev/twig/bc/fc/8a739a3063e0118d596e9d98d7fec07764a6f69cef9435ae410ddb48f46b.php
@@ -0,0 +1,153 @@
+<?php
+
+/* AcmeDemoBundle::layout.html.twig */
+class __TwigTemplate_bcfc8a739a3063e0118d596e9d98d7fec07764a6f69cef9435ae410ddb48f46b extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        try {
+            $this->parent = $this->env->loadTemplate("TwigBundle::layout.html.twig");
+        } catch (Twig_Error_Loader $e) {
+            $e->setTemplateFile($this->getTemplateName());
+            $e->setTemplateLine(1);
+
+            throw $e;
+        }
+
+        $this->blocks = array(
+            'head' => array($this, 'block_head'),
+            'title' => array($this, 'block_title'),
+            'body' => array($this, 'block_body'),
+            'content_header' => array($this, 'block_content_header'),
+            'content_header_more' => array($this, 'block_content_header_more'),
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "TwigBundle::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_head($context, array $blocks = array())
+    {
+        // line 4
+        echo "    <link rel=\"icon\" sizes=\"16x16\" href=\"";
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("favicon.ico"), "html", null, true);
+        echo "\" />
+    <link rel=\"stylesheet\" href=\"";
+        // line 5
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/acmedemo/css/demo.css"), "html", null, true);
+        echo "\" />
+";
+    }
+
+    // line 8
+    public function block_title($context, array $blocks = array())
+    {
+        echo "Demo Bundle";
+    }
+
+    // line 10
+    public function block_body($context, array $blocks = array())
+    {
+        // line 11
+        echo "    ";
+        $context['_parent'] = (array) $context;
+        $context['_seq'] = twig_ensure_traversable($this->getAttribute($this->getAttribute($this->getAttribute((isset($context["app"]) ? $context["app"] : $this->getContext($context, "app")), "session", array()), "flashbag", array()), "get", array(0 => "notice"), "method"));
+        foreach ($context['_seq'] as $context["_key"] => $context["flashMessage"]) {
+            // line 12
+            echo "        <div class=\"flash-message\">
+            <em>Notice</em>: ";
+            // line 13
+            echo twig_escape_filter($this->env, $context["flashMessage"], "html", null, true);
+            echo "
+        </div>
+    ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['flashMessage'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 16
+        echo "
+    ";
+        // line 17
+        $this->displayBlock('content_header', $context, $blocks);
+        // line 26
+        echo "
+    <div class=\"block\">
+        ";
+        // line 28
+        $this->displayBlock('content', $context, $blocks);
+        // line 29
+        echo "    </div>
+
+    ";
+        // line 31
+        if (array_key_exists("code", $context)) {
+            // line 32
+            echo "        <h2>Code behind this page</h2>
+        <div class=\"block\">
+            <div class=\"symfony-content\">";
+            // line 34
+            echo (isset($context["code"]) ? $context["code"] : $this->getContext($context, "code"));
+            echo "</div>
+        </div>
+    ";
+        }
+    }
+
+    // line 17
+    public function block_content_header($context, array $blocks = array())
+    {
+        // line 18
+        echo "        <ul id=\"menu\">
+            ";
+        // line 19
+        $this->displayBlock('content_header_more', $context, $blocks);
+        // line 22
+        echo "        </ul>
+
+        <div style=\"clear: both\"></div>
+    ";
+    }
+
+    // line 19
+    public function block_content_header_more($context, array $blocks = array())
+    {
+        // line 20
+        echo "                <li><a href=\"";
+        echo $this->env->getExtension('routing')->getPath("_demo");
+        echo "\">Demo Home</a></li>
+            ";
+    }
+
+    // line 28
+    public function block_content($context, array $blocks = array())
+    {
+    }
+
+    public function getTemplateName()
+    {
+        return "AcmeDemoBundle::layout.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  135 => 28,  128 => 20,  125 => 19,  118 => 22,  116 => 19,  113 => 18,  110 => 17,  102 => 34,  98 => 32,  96 => 31,  92 => 29,  90 => 28,  86 => 26,  84 => 17,  81 => 16,  72 => 13,  69 => 12,  64 => 11,  61 => 10,  55 => 8,  49 => 5,  44 => 4,  41 => 3,  11 => 1,);
+    }
+}
