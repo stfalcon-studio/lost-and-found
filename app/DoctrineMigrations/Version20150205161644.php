f4bd871dd9a4d899b339200a37c9dc5ca9c7a467
LogansUA <logansoleg@gmail.com>
2015-02-06 16:53:37 +0200

    Fix fail

--- app/DoctrineMigrations/Version20150205161644.php
+++ app/DoctrineMigrations/Version20150205161644.php
@@ -23,6 +23,6 @@ class Version20150205161644 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE items CHANGE areaType areaType VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci');
+        $this->addSql('ALTER TABLE items CHANGE areaType areaType ENUM(\'polygon\', \'found\', \'circle\', \'marker\') NOT NULL COMMENT \'(DC2Type:ItemAreaTypeType)\'');
     }
 }
