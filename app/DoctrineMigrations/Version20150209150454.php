815cf84abf9847496e776c93fc2a74930f92f8d6
svatok13 <svatok13@gmail.com>
2015-02-09 15:05:52 +0200

    Migration fix

--- app/DoctrineMigrations/Version20150209150454.php
+++ app/DoctrineMigrations/Version20150209150454.php
@@ -8,14 +8,16 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20150209120057 extends AbstractMigration
+class Version20150209150454 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE categories ADD path VARCHAR(3000) DEFAULT NULL, DROP lft, DROP rgt, DROP root, CHANGE lvl lvl INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE categories ADD parent_id INT DEFAULT NULL, ADD path VARCHAR(3000) DEFAULT NULL, ADD lvl INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE categories ADD CONSTRAINT FK_3AF34668727ACA70 FOREIGN KEY (parent_id) REFERENCES categories (id) ON DELETE CASCADE');
+        $this->addSql('CREATE INDEX IDX_3AF34668727ACA70 ON categories (parent_id)');
     }
 
     public function down(Schema $schema)
@@ -23,6 +25,8 @@ class Version20150209120057 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE categories ADD lft INT NOT NULL, ADD rgt INT NOT NULL, ADD root INT DEFAULT NULL, DROP path, CHANGE lvl lvl INT NOT NULL');
+        $this->addSql('ALTER TABLE categories DROP FOREIGN KEY FK_3AF34668727ACA70');
+        $this->addSql('DROP INDEX IDX_3AF34668727ACA70 ON categories');
+        $this->addSql('ALTER TABLE categories DROP parent_id, DROP path, DROP lvl');
     }
 }
