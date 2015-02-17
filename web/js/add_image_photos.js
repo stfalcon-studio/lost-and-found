f4654164148d928d726415a3d6ff6b66eb586f79
svatok13 <svatok13@gmail.com>
2015-02-17 16:06:42 +0200

    Add photos in item creators Add photos in item details

--- web/js/add_image_photos.js
+++ web/js/add_image_photos.js
@@ -1,13 +1,16 @@
 var $collectionHolder;
 
-console.log('hello');
 // setup an "add a tag" link
 var $addPhotoLink = $('<a href="#" class="add_photo_link">Add a photo</a>');
 var $newLinkLi = $('<p></p>').append($addPhotoLink);
 
 jQuery(document).ready(function() {
     // Get the ul that holds the collection of tags
-    $collectionHolder = $('ul.photos');
+    var type = $('.type').data().type;
+    if (type == 'lost')
+        $collectionHolder = $('#lost_item_photos');
+    else
+        $collectionHolder = $('#found_item_photos');
 
     // add the "add a tag" anchor and li to the tags ul
     $collectionHolder.append($newLinkLi);
@@ -30,16 +33,25 @@ function addPhotoForm($collectionHolder, $newLinkLi) {
     var prototype = $collectionHolder.data('prototype');
 
     // get the new index
-    var index = $collectionHolder.data('index');
-
+     var index = $collectionHolder.data('index');
+     if (index == 0)
+        index = 1;
     // Replace '__name__' in the prototype's HTML to
     // instead be a number based on how many items we have
-    var newForm = prototype.replace(/__name__/g, index);
-
+    var newForm = prototype.replace(/__name__label__/g, '<i>'+index + ' photo</i>');
     // increase the index with one for the next item
     $collectionHolder.data('index', index + 1);
 
     // Display the form in the page in an li, before the "Add a tag" link li
-    var $newFormLi = $('<li></li>').append(newForm);
+    var $newFormLi = $('<li style="list-style-type: none;"></li>').append(newForm);
+    $newFormLi.append('<a href="#" class="remove-photo">Delete a photo</a>');
     $newLinkLi.before($newFormLi);
+
+    $('.remove-photo').click(function(e) {
+        e.preventDefault();
+
+        $(this).parent().remove();
+
+        return false;
+    });
 }
