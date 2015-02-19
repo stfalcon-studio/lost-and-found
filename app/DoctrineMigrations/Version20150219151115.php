daf1a41cd15af2afbd235f7ee2337ee793528e26
Myrkotyn <prohorovychua@gmail.com>
2015-02-19 15:24:37 +0200

    Fix migrations

--- app/DoctrineMigrations/Version20150219151115.php
+++ app/DoctrineMigrations/Version20150219151115.php
@@ -8,14 +8,15 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20150219105310 extends AbstractMigration
+class Version20150219151115 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE faq_translations ADD object_class VARCHAR(255) NOT NULL, ADD foreign_key VARCHAR(64) NOT NULL');
+        $this->addSql('CREATE TABLE faq_translations (id INT AUTO_INCREMENT NOT NULL, object_id INT DEFAULT NULL, locale VARCHAR(8) NOT NULL, field VARCHAR(32) NOT NULL, content LONGTEXT DEFAULT NULL, INDEX IDX_99569DA2232D562B (object_id), UNIQUE INDEX faq_translation_unique_idx (locale, object_id, field), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE faq_translations ADD CONSTRAINT FK_99569DA2232D562B FOREIGN KEY (object_id) REFERENCES faq (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema)
@@ -23,6 +24,6 @@ class Version20150219105310 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE faq_translations DROP object_class, DROP foreign_key');
+        $this->addSql('DROP TABLE faq_translations');
     }
 }
