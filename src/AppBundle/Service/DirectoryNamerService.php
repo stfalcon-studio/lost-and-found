fb91ce3cafa2bb586869f9996ddfa814148a7f36
svatok13 <svatok13@gmail.com>
2015-05-20 11:21:08 +0300

    Add directory namer

---
+++ src/AppBundle/Service/DirectoryNamerService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Service;
+
+use Vich\UploaderBundle\Mapping\PropertyMapping;
+use Vich\UploaderBundle\Naming\DirectoryNamerInterface;
+
+/**
+ * Class DirectoryNamerService
+ *
+ * @author Yuri Svatok <svatok13@gmail.com>
+ */
+class DirectoryNamerService implements DirectoryNamerInterface
+{
+
+    protected $securityContext;
+
+    /**
+     * @param object          $object
+     * @param PropertyMapping $mapping
+     *
+     * @return string
+     */
+    public function directoryName($object, PropertyMapping $mapping)
+    {
+        /** @var \DateTime $date */
+        $date = $object->getCreatedAt();
+
+        return $date->format('Y').'/'.$date->format('m').'/'.$date->format('d');
+    }
+}
